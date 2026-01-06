931e89ea2a005404934f1d259669a4ac36caef0a
Aaron Blancaflor <ablancaflor@runding.com>
2026-01-06 14:44:21 -0800

    More boiler plate for about page and galleries page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function About() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Home Page</p>
+      <p>About Page</p>
     </div>
   );
 }
--- app/galleries/page.tsx
+++ app/galleries/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Galleries() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Home Page</p>
+      <p>Galleries Page</p>
     </div>
   );
 }
