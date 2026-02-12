267a9fae1e7d6f91c5324337b967a56511cdd663
Aaron Blancaflor <ablancaflor@runding.com>
2026-02-11 16:01:01 -0800

    Added boilerplate for nestjs backend services.

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Hero } from "./components/Hero";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Hero />
-    </div>
-  );
-}
