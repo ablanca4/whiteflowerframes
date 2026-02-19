a79680fc003b5add648001580a8e8e6a393f99f6
Aaron Blancaflor <ablancaflor@runding.com>
2026-02-19 11:50:06 -0800

    Recreated backend for users, and images

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ImageController } from './Controllers/image.controller';
+import { ImagesController } from './images/images.controller';
+import { ImagesService } from './images/images.service';
 import { ImagesModule } from './images/images.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ImagesModule, UsersModule],
-  controllers: [AppController, ImageController],
-  providers: [AppService],
+  controllers: [AppController, ImagesController],
+  providers: [AppService, ImagesService],
 })
 export class AppModule {}
--- backend/src/images/images.controller.ts
+++ backend/src/images/images.controller.ts
@@ -1,14 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller('images')
-export class ImagesController {
-  @Post()
-  create(): string {
-    return 'this uploads a new image';
-  }
-
-  @Get()
-  findAll(): string {
-    return 'This gets all images';
-  }
-}
+export class ImagesController {}
