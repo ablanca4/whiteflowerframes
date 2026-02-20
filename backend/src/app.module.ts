42c5bb7836a84adb76c86a6bee6b563e2926b502
Aaron Blancaflor <aaron.j.blancaflor@gmail.com>
2026-02-19 21:14:31 -0800

    Cleaned up starter code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ImagesController } from './images/images.controller';
 import { ImagesService } from './images/images.service';
 import { ImagesModule } from './images/images.module';
@@ -8,7 +6,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ImagesModule, UsersModule],
-  controllers: [AppController, ImagesController],
-  providers: [AppService, ImagesService],
+  controllers: [ImagesController],
+  providers: [ImagesService],
 })
-export class AppModule {}
+export class AppModule { }
